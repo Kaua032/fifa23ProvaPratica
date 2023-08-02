f7fa546beb8cf2e4b6f9e4fcf5db1fa20acce80c
Kauã Xavier <104097066+Kaua032@users.noreply.github.com>
2023-08-02 07:49:23 -0300

    feat: Update kauaXavier.sql

--- kauaXavier.sql
+++ kauaXavier.sql
@@ -9,7 +9,7 @@ CREATE TABLE tb_usuarios(
     nome VARCHAR(50) NOT NULL,
     email VARCHAR(50) NOT NULL,
     senha VARCHAR(16) NOT NULL,
-    tipo VARCHAR(10) NOT NULL
+    tipo VARCHAR(10) ENUM('admin', 'user') NOT NULL
 );
 
 # Tabela Uniforme
